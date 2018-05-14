3718e282f837e539694c31a2a393972a4fff4ba0
Robotistan.com <yusuf@bomec.com.tr>
2018-05-14 11:25:44 +0300

    Add files via upload

---
+++ Otomatik Perde/NRF24_Test_Alici.ino
@@ -0,0 +1,46 @@
+#include <Stepper.h>
+#include <SPI.h>
+#include "nRF24L01.h"
+#include "RF24.h"           //Modül ile ilgili kütüphaneleri ekliyoruz
+
+int mesaj[1];
+int led = 2;
+const uint64_t kanal = 1011;
+
+RF24 alici(9,10);
+
+#define STEPS 4096
+
+Stepper stepper(STEPS, 7, 6, 5, 4);
+
+void setup(void){
+ Serial.begin(9600);
+ 
+ alici.begin();
+ alici.openReadingPipe(1,kanal);
+ alici.startListening();
+ pinMode(led, OUTPUT);
+ stepper.setSpeed(2);
+ }
+
+void loop(void){
+ Serial.println("mesaj yok");
+ if(alici.available())
+ {
+    
+  alici.read(mesaj, 1);      
+  Serial.println(mesaj[0]);
+     if (mesaj[0] == 100)
+     {
+
+      stepper.step(4096);
+      }
+      if(mesaj[0] == 200){
+     
+      stepper.step(-4096);
+      }
+     
+     delay(10);
+   }
+   delay(300);
+ }
---
+++ Otomatik Perde/NRF24_Test_Verici.ino.ino
@@ -0,0 +1,43 @@
+//#include  <SPI.h> 
+#include "nRF24L01.h"
+#include "RF24.h"     //Modül ile ilgili kütüphaneleri ekliyoruz
+
+int mesaj[1];      //mesaj isminde bir dizi tanımlıyoruz
+RF24 verici(9,10);     //kütüphane tarafından kullanılacak pinleri tanımlıyoruz
+const uint64_t kanal = 1011; //kanalı tanımlıyoruz  
+int buton = 7;      //butonun bağlı olduğu dijital pin
+
+#define buton_a 2
+#define buton_k 3
+
+void setup(void)
+{
+ verici.begin();       //nrf yi başlatıyoruz
+ verici.openWritingPipe(kanal);  //kanal id sı tanımlanıyor
+ Serial.begin(9600);
+ pinMode(buton_a, INPUT_PULLUP);
+ pinMode(buton_k, INPUT_PULLUP);
+} 
+
+
+void loop(void)
+{
+
+if(digitalRead(buton_a) == 0){
+
+  mesaj[0] = 100; 
+ verici.write(mesaj, 1);   //mesaj değişkeni yollanıyor
+ Serial.println("mesaj gönderildi");
+  delay(200);
+  
+}
+
+ if(digitalRead(buton_k) == 0){
+
+  mesaj[0] = 200; 
+ verici.write(mesaj, 1);   //mesaj değişkeni yollanıyor
+ Serial.println("mesaj gönderildi");
+  delay(200);
+  
+}
+ }
