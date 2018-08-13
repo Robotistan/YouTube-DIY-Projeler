27ba2cbec756ef4e7ef61acc177b2ccca381dc47
Robotistan.com <yusuf@bomec.com.tr>
2018-08-13 14:04:49 +0300

    Add files via upload

---
+++ Arduino Dersleri/Ders-12 Joystick ile Servo Motor Kontrol/Servo_Motor_Kontrol.ino
@@ -0,0 +1,19 @@
+#include <Servo.h>
+
+Servo motor;
+
+int deger;
+int derece;
+
+void setup() {
+  motor.attach(3);
+
+}
+
+void loop() {
+
+  deger = analogRead(A0);
+  derece = map(deger, 0,1023,0,180);
+  motor.write(derece);
+
+}
