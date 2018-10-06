488aa498fa442e546e9fdc5171909a4a759d63d5
Robotistan.com <yusuf@bomec.com.tr>
2018-10-06 22:23:45 +0300

    Add files via upload

---
+++ Arduino Dersleri/Ders-18 Gaz Alarmi/gaz_kacak.ino
@@ -0,0 +1,22 @@
+int esikDegeri = 400;               //Gaz eşik değerini belirliyoruz.    
+int buzzerPin = 9;                  //Buzzerın takılacağı pin
+int deger;                          //Sensörden okunan değer
+
+void setup() {
+  pinMode(buzzerPin, OUTPUT);       //Buzzer pinimizi çıkış olarak ayarlıyoruz.
+}
+
+void loop() {
+  deger = analogRead(A0);           //Sensörden analog değer okuyoruz.
+  if(deger > esikDegeri){           //Sensörden okunan değer eşik değerinden büyükse çalışır.
+    digitalWrite(buzzerPin, HIGH);
+    delay(100);
+    digitalWrite(buzzerPin, LOW);
+    delay(100);
+  }
+  else{                             //Sensörden okunan değer eşik değerinin altındaysa çalışır.
+    digitalWrite(buzzerPin, LOW);
+  }
+}
+
+
