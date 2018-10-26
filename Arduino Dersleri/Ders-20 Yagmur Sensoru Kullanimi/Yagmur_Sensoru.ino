17c0824e4b899f0894b6c7cbbf141638f45c9994
Robotistan.com <yusuf@bomec.com.tr>
2018-10-26 08:42:07 +0300

    Add files via upload

---
+++ Arduino Dersleri/Ders-20 Yagmur Sensoru Kullanimi/Yagmur_Sensoru.ino
@@ -0,0 +1,22 @@
+int sensorPin = A0;                //Sensörü bağlayacağımız pin
+int esikDegeri = 100;              //Su miktarı için eşik değeri
+int buzzerPin = 8;                 //Buzzerı bağlayacağımız pin
+int veri;                          //Sensörden okuduğumuz değer
+
+void setup() {
+  pinMode(buzzerPin, OUTPUT);      //Buzzer bağladığımız pini OUTPUT olarak ayarlıyoruz.  
+}
+void loop() {
+  veri = analogRead(sensorPin);    //Sensörden analog veriyi okuyoruz.
+  if(veri > esikDegeri){           //Sensör verisi eşik değerini geçerse if içerisindeki kodlar uygulanır.
+    digitalWrite(buzzerPin, HIGH); 
+    delay(100);
+    digitalWrite(buzzerPin, LOW);
+    delay(100);
+  }
+  else{                            //Sensör verisi eşik değerinden küçük olursa if içerisindeki kodlar uygulanır.
+    digitalWrite(buzzerPin, LOW);
+  }
+}
+
+
